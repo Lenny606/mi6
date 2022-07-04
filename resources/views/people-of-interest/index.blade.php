722526b2c70940c1e813566b4606d9f944627d6a
Tomáš Kravčík <thomas.kravcik@gmail.com>
2022-07-04 16:13:32 +0200

    react listing

--- resources/views/people-of-interest/index.blade.php
+++ resources/views/people-of-interest/index.blade.php
@@ -4,16 +4,7 @@
 {{-- <div class="main-panel">
     <p>Welcome People Of Interest</p>
 </div> --}}
-    {{-- @foreach ($people as $person)
-        <p>{{ $person->name }} is {{ $person->occupation }}</p>
-        <p>Known aliases:</p>
-        <ul>
-            @foreach ($person->aliases as $alias)
-            <li>{{ $alias->alias }}</li>
-            @endforeach
-        </ul>
-    @endforeach --}}
-    <div id="root">
+    <div class="main-panel" id="root">
         <script src="{{ mix("/js/people-of-interest.js") }}"></script>
     </div>
 @endsection
