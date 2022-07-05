2b7769f885f8a7b2487b5e011ad916f6ff975671
BenHbrt <benherbert1983@googlemail.com>
2022-07-05 09:41:36 +0200

    Setting the statuses

--- resources/js/PeopleOfInterest/StatusFilter.js
+++ resources/js/PeopleOfInterest/StatusFilter.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import { useState, useEffect } from "react";
 
-const StatusFilter = () => {
+const StatusFilter = ({ selectedStatus, setSelectedStatus}) => {
+
+    const [statuses, setStatuses] = useState([])
+
+    const loadStatuses = async () => {
+
+        const response = await fetch('/api/statuses')
+        const parsedResponse = await response.json();
+
+        setStatuses(parsedResponse);
+    }
+    
+    useEffect(() => {
+        loadStatuses()
+    }, [])
 
     return (
         <div className='status-filter'>
             This is the status filter
+            {statuses.map((status) => {
+                <div onClick={setStatuses(status.id)} className={ 'status-filter__status' }>
+                { status.name }
+            </div>
+            })}
         </div>
     )
 
