c59befe5f569b7ab849acf601c8fbc87af973927
BenHbrt <benherbert1983@googlemail.com>
2022-07-05 14:48:28 +0200

    changing the models

--- database/migrations/2022_07_05_120522_create_table_missions_people.php
+++ database/migrations/2022_07_05_120522_create_table_missions_people.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMissionPerson extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -15,7 +15,7 @@ class CreateMissionPerson extends Migration
     {
         Schema::create('mission_person', function (Blueprint $table) {
      
-    $table->foreignId('mission_id');
+            $table->foreignId('mission_id');
             $table->foreignId('person_id');
           
         });
