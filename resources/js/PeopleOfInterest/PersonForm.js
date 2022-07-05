2789b840199278a1e19b78267a03d90aa266ea16
Blake Dodgen <blakedodgen@gmail.com>
2022-07-05 16:08:33 +0200

    form works ...kinda

--- resources/js/PeopleOfInterest/PersonForm.js
+++ resources/js/PeopleOfInterest/PersonForm.js
@@ -2,21 +2,22 @@ import { useState } from 'react';
 
 const PersonForm = ({ name }) => {
 
-    const [data, setData] = useState('')
+    const [data, setData] = useState([])
 
     const handleClick = (event) => {
         event.preventDefault();
         console.log(event.target.value)
         setData(data => {
-            return ({...data, 
-                [event.target.name]:event.target.name
+            return ({
+                ...data,
+                [event.target.name]: event.target.name
             });
         });
     }
 
     return (
-        <form action='' method='post' onSubmit={ handleClick }>
-            <input type="text" name="name" value = {name} />
+        <form action='' method='post' onSubmit={handleClick}>
+            <input type="text" name="name" defaultValue={name} />
             <input type="submit" />
         </form>
     )
